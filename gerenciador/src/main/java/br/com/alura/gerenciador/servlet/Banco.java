ca226099c8d3b6f6a5fedb1f7ac0d29ae838fb37
Ideraldo <67620423+Ideraldo@users.noreply.github.com>
2021-05-08 22:51:00 -0300

    Initial commit

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,61 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+
+	public void adiciona(Empresa empresa) {
+		Banco.lista.add(empresa);
+		empresa.setId(Banco.chaveSequencial++);
+	}
+	
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+		
+		/*
+		 * O uso de iterator permite alterar a lista ao mesmo tempo que a percorremos
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				lista.remove(empresa);
+			}
+		}*/
+	}
+
+	public Empresa buscaEmpresaPeloId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+
+}
